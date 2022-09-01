e6719f55e04bfe45b6a94794af6f1bad1d704b70
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-01 18:55:40 -0400

    input API on JS file

--- assets/script/weather.js
+++ assets/script/weather.js
@@ -5,12 +5,30 @@ formulario.addEventListener("submit", function(event) {
     const inputCidade = document.getElementById("campo__busca");
     const apiKey = "13e2d569c4b820d2080738f98d8b6b1e";
     const inputVal = inputCidade.value;
+    const list = document.getElementById("list");
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            const {main, name, sys, weather } = data;
+            const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+
+            const li = document.createElement("li");
+            li.classList.add("dados__cidade");
+            const paginaWeb = `
+                <h2 class="nome__cidade" data-name="${name}, ${sys.country}">
+                    <span class="cidade">${name}</span>
+                    <sup class="abrev__pais">${sys.country}</sup>
+                </h2>
+                <span class="temperatura__cidade">${Math.round(main.temp)}<sup class="celsius__simbolo">°c</sup>
+                </span>
+                <figure>
+                    <img src=${icon} Alt=${weather[0]["main"]} class="icone__previsao--tempo">
+                    <figcaption class="descricao_tempo">${weather[0]["description"]}</figcaption>
+                </figure>
+            `;
+            li.innerHTML = paginaWeb;
+            list.appendChild(li);
             console.log(data);
-        })
-
-
+        })  
 })
